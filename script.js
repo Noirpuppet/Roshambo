1c1ec38ec93cd5f87617a0b344f8de4a40c66373
Noirpuppet <chenin2808@gmail.com>
2022-11-14 16:53:47 +0530

    added a display for winner check playround

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const option = ["rock", "paper", "scissor"];
     const random = option[Math.floor(Math.random() * option.length)];
@@ -11,16 +14,22 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return "It's a Draw!";
     }else if (playerSelection === "rock" && computerSelection === "scissor"){
+        playerScore++
         return "You Win! Rock beats Scissor";
     }else if (playerSelection === "rock" && computerSelection === "paper"){
+        computerScore++
         return "You Lose! Paper beats Rock";
     }else if (playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++
         return "You Win! Paper beats Rock";
     }else if (playerSelection === "paper" && computerSelection === "scissor"){
+        computerScore++
         return "You Lose! Scissor beats Paper";
     }else if (playerSelection === "scissor" && computerSelection === "paper"){
+        playerScore++
         return "You Win! Scissor beats Paper";
     }else if (playerSelection === "scissor" && computerSelection === "rock"){
+        computerScore++
         return "You Lose! Rock beats Scissor";
     }
 }
@@ -30,6 +39,13 @@ function game(){
         const playerSelection = prompt("choose Your weapon!");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+    }
+    if(playerScore > computerScore){
+        console.log("You are the winner!");
+    }else if(computerScore > playerScore){
+        console.log("Computer is the winner!");
+    }else{
+        console.log("It's a Draw!")
     }
 }
 
