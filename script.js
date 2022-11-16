2b5f602b6b9ff0f539ca7fd7c4d7182cf9780a19
Noirpuppet <chenin2808@gmail.com>
2022-11-16 16:58:15 +0530

    completed with UI

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+const container = document.querySelector('.container');
+const rockbtn = document.querySelector('.rock');
+const paperbtn = document.querySelector('.paper');
+const scissorbtn = document.querySelector('.scissor');
+const displayScore = document.querySelector('.display');
+const playerPoint = document.querySelector('.playerPoint');
+const computerPoint = document.querySelector('.computerPoint');
+
+
+
+
+
+rockbtn.addEventListener('click', () =>{
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    checkWinner(playerScore, computerScore);
+});
+
+paperbtn.addEventListener('click', () =>{
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    checkWinner(playerScore, computerScore);
+});
+
+scissorbtn.addEventListener('click', () =>{
+    const playerSelection = 'scissor';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    checkWinner(playerScore, computerScore);
+});
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = '-------------------------------'
+
+container.appendChild(content);
+
+
+
+const updateScore = function(playerScore, computerScore){
+    playerPoint.innerText = `Player Score: ${playerScore}`;
+
+    computerPoint.innerText = `Computer Score: ${computerScore}`;
+}
+
+
+const checkWinner = function(playerScore, computerScore){
+    console.log(playerScore, computerScore);
+    if(playerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.innerHTML = "Congrats! you are the winner";
+        container.appendChild(h2);
+    }else if(computerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.innerHTML = "You Lose! Computer is the winner";
+        container.appendChild(h2);
+    }
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -12,41 +80,41 @@ getComputerChoice();
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
-        return "It's a Draw!";
+        content.innerHTML = "It's a Draw!";
     }else if (playerSelection === "rock" && computerSelection === "scissor"){
         playerScore++
-        return "You Win! Rock beats Scissor";
+        content.innerHTML = "You Win! Rock beats Scissor";
     }else if (playerSelection === "rock" && computerSelection === "paper"){
         computerScore++
-        return "You Lose! Paper beats Rock";
+        content.innerHTML = "You Lose! Paper beats Rock";
     }else if (playerSelection === "paper" && computerSelection === "rock"){
         playerScore++
-        return "You Win! Paper beats Rock";
+        content.innerHTML = "You Win! Paper beats Rock";
     }else if (playerSelection === "paper" && computerSelection === "scissor"){
         computerScore++
-        return "You Lose! Scissor beats Paper";
+        content.innerHTML = "You Lose! Scissor beats Paper";
     }else if (playerSelection === "scissor" && computerSelection === "paper"){
         playerScore++
-        return "You Win! Scissor beats Paper";
+        content.innerHTML = "You Win! Scissor beats Paper";
     }else if (playerSelection === "scissor" && computerSelection === "rock"){
         computerScore++
-        return "You Lose! Rock beats Scissor";
+        content.innerHTML = "You Lose! Rock beats Scissor";
     }
 }
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("choose Your weapon!");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    if(playerScore > computerScore){
-        console.log("You are the winner!");
-    }else if(computerScore > playerScore){
-        console.log("Computer is the winner!");
-    }else{
-        console.log("It's a Draw!")
-    }
-}
+// function game(){
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt("choose Your weapon!");
+//         const computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+//     if(playerScore > computerScore){
+//         console.log("You are the winner!");
+//     }else if(computerScore > playerScore){
+//         console.log("Computer is the winner!");
+//     }else{
+//         console.log("It's a Draw!")
+//     }
+// }
 
-game();
+// game();
